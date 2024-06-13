2e41c1c02fa42aef9080fd3cf6df7dc5be407ff4
SimaThe1Ferreira <simaotheferreira@gmail.com>
2024-06-13 12:13:31 +0100

    Update do.sh

--- do.sh
+++ do.sh
@@ -4,13 +4,19 @@ msg_error() {
 	echo "Invalid command!"
 }
 run() {
-	qemu-system-i386 -m 1024 -cpu qemu32 -bios BIOS -drive if=none,id=usbstick,format=raw,file=bootable_drive.bin -usb -device usb-ehci,id=ehci -device usb-storage,bus=ehci.0,drive=usbstick -device usb-kbd
+	echo "Running..."
+	qemu-system-i386 -m 1024 -cpu qemu32 -drive if=none,id=usbstick,format=raw,file=bootable_drive.bin -usb -device nec-usb-xhci,id=xhci -device usb-storage,bus=xhci.0,drive=usbstick
 }
 build() {
+	echo "Assembling..."
 	as -o boot_chunk.o boot_chunk.s
+	echo "Linking..."
 	ld -T memory_map.ld --oformat "binary" -o OS.bin boot_chunk.o
+	echo "Creating drive image..."
 	dd if=/dev/zero of=bootable_drive.bin bs=512 count=2880
+	echo "Inserting bootloader..."
 	dd if=OS.bin of=bootable_drive.bin seek=0 conv=notrunc
+	echo "Clearing cache..."
 	rm boot_chunk.o OS.bin
 }
 if [ "$#" -gt 1 ]
@@ -25,20 +31,18 @@ then
 	exit 1
 fi
 case "$1" in
-    "build")
-        build
-        ;;
-    "run")
-        run
-        ;;
-    "debug")
-        bochs -f options.bochs
-        ;;
-    "install")
-        apt update && apt install qemu-system-x86 hexedit binutils gcc bochs bochsbios vgabios bochs-x
-        ;;
-    *)
-        msg_error
-        ;;
+	"build")
+		build
+	;;
+	"run")
+		run
+	;;
+	"install")
+		sudo apt update
+		sudo apt install qemu-system-x86 hexedit binutils gcc bochs bochsbios vgabios bochs-x
+	;;
+	*)
+		msg_error
+	;;
 esac
 
